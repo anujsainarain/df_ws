7872a54391d8f1609e7984deecbf5904f0d9b44f
Anuj <anujnarain@nyu.edu>
2024-12-19 17:17:21 -0500

    Docker - Decision Maker Node

--- src/decision_maker_node/models/download_model.sh
+++ src/decision_maker_node/models/download_model.sh
@@ -1,12 +1,33 @@
 #!/bin/bash
 
-# URL to download
-FILE_URL="https://drive.google.com/uc?id=1MI0UPvoyLGLO2JgPjnhIOe9PsJT7qTRu&export=download"
+FILE_ID="1MI0UPvoyLGLO2JgPjnhIOe9PsJT7qTRu"
+OUTPUT_PATH="/root/df_ws/src/decision_maker_node/models/llama-2-7b-chat.Q5_K_M.gguf"
 
-# Target folder
-OUTPUT_PATH="src/decision_maker_node/models/llama-2-7b-chat.Q5_K_M-002.gguf"
+echo "Downloading the file..."
+gdown --id "$FILE_ID" -O "$OUTPUT_PATH"
 
-echo "Downloading the GGUF model file..." # Heads up
-wget --no-check-certificate "$FILE_URL" -O "$OUTPUT_PATH"
+if [ $? -eq 0 ]; then
+    echo "Download complete."
+else
+    echo "Failed."
+    exit 1
+fi
 
-echo "Download complete."
+
+
+
+
+# # URL to download
+# # FILE_URL="https://drive.google.com/uc?id=1MI0UPvoyLGLO2JgPjnhIOe9PsJT7qTRu&export=download"
+# # FILE_URL="https://drive.google.com/uc?id=1qG0xuBa_0iV3eUgBSfeAmsXWaQrUvI9L&export=download" #Paddle_ball
+# FILE_ID = "1qG0xuBa_0iV3eUgBSfeAmsXWaQrUvI9L"
+
+# # Target folder
+# # OUTPUT_PATH="src/decision_maker_node/models/llama-2-7b-chat.Q5_K_M-002.gguf"
+# OUTPUT_PATH="/root/df_ws/src/decision_maker_node/models/Paddle_ball.py"
+
+
+# echo "Downloading the GGUF model file..." # Heads up
+# wget --no-check-certificate "$FILE_URL" -O "$OUTPUT_PATH"
+
+# echo "Download complete."
