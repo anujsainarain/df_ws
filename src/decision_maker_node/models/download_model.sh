1d8593deded27c95bb692cac46c611cbf3a796b2
Anuj <anujnarain@nyu.edu>
2024-12-22 15:08:36 -0500

    Fixed: Whisper_cpp issue

--- src/decision_maker_node/models/download_model.sh
+++ src/decision_maker_node/models/download_model.sh
@@ -12,22 +12,3 @@ else
     echo "Failed."
     exit 1
 fi
-
-
-
-
-
-# # URL to download
-# # FILE_URL="https://drive.google.com/uc?id=1MI0UPvoyLGLO2JgPjnhIOe9PsJT7qTRu&export=download"
-# # FILE_URL="https://drive.google.com/uc?id=1qG0xuBa_0iV3eUgBSfeAmsXWaQrUvI9L&export=download" #Paddle_ball
-# FILE_ID = "1qG0xuBa_0iV3eUgBSfeAmsXWaQrUvI9L"
-
-# # Target folder
-# # OUTPUT_PATH="src/decision_maker_node/models/llama-2-7b-chat.Q5_K_M-002.gguf"
-# OUTPUT_PATH="/root/df_ws/src/decision_maker_node/models/Paddle_ball.py"
-
-
-# echo "Downloading the GGUF model file..." # Heads up
-# wget --no-check-certificate "$FILE_URL" -O "$OUTPUT_PATH"
-
-# echo "Download complete."
